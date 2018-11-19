ede633da9bffdad7a020cb957f1f5a36ded89222
dominic_lian <dominic_lian@qq.com>
2018-11-19 20:59:45 +0800

    update

---
+++ 2. OOP/Composition/src/com/dominic/Main.java
@@ -0,0 +1,21 @@
+package com.dominic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(02, 20, 5);
+	    Case theCase = new Case("2208", "Dell", "240", dimensions);
+
+	    Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2560, 1440));
+
+	    Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+	    PC thePC = new PC(theCase,theMonitor, theMotherboard);
+
+//	    thePC.getMonitor().drawPixelAt(1500, 1200 ,"red");
+//	    thePC.getMotherboard().loadProgram("Windows 1.0");
+//	    thePC.getTheCase().pressPowerButton();
+
+        thePC.powerUp();
+    }
+}
