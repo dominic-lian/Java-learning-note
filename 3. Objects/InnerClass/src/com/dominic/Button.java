27c922c74460d01a3d4ef47b39df22ae15fea26a
dominic_lian <dominic_lian@qq.com>
2018-12-14 15:43:52 +0800

    update

---
+++ 3. Objects/InnerClass/src/com/dominic/Button.java
@@ -0,0 +1,26 @@
+package com.dominic;
+
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public OnClickListener getOnClickListener() {
+        return onClickListener;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
